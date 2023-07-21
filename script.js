b0908c25c22b94d7b06dc1f814dd38c69bbb6ca5
Gaspar Bonari <gasparbonari@gmail.com>
2023-07-21 16:19:12 +0100

    .

--- script.js
+++ script.js
@@ -53,21 +53,12 @@ const draw = function()
     }
 
     const length = end.x - start.x;
-    const arcRadius = length * 0.05;
     const initialArcRadius = length * 0.05;
     const circleRadius = length * 0.0065;
     const spacing = (length / 2 - initialArcRadius) / arcs.length;
     const currentTime = new Date().getTime();
     const elapsedTime = (currentTime - startTime) / 1000;
-    const velocity = 0.5;
-    const maxAngle = 2 * Math.PI;
-    const distance = Math.PI + (elapsedTime * velocity);
-    const modDistance = distance % maxAngle;
-    
-    const adjustDistance = modDistance >= Math.PI ? modDistance : maxAngle - modDistance;
-    
-    const x = center.x + arcRadius * Math.cos(adjustDistance);
-    const y = center.y + arcRadius * Math.sin(adjustDistance);
+  
 
     pen.strokeStyle = "white";
     pen.lineWidth = 6;
@@ -80,16 +71,25 @@ const draw = function()
 
     arcs.forEach((arc, index) => 
     {
-        const arcRadiuss = initialArcRadius + (index * spacing)
+        const numberOfLoops = 50 - index;
+        const oneFullLoop = 2 * Math.PI;
+        const velocity = (oneFullLoop * numberOfLoops) / 900;
+        const maxAngle = 2 * Math.PI;
+        const distance = Math.PI + (elapsedTime * velocity);
+        const modDistance = distance % maxAngle;
+        const adjustDistance = modDistance >= Math.PI ? modDistance : maxAngle - modDistance;
+        const arcRadius = initialArcRadius + spacing * (index + 1);
+        const x = center.x + arcRadius * Math.cos(adjustDistance);
+        const y = center.y + arcRadius * Math.sin(adjustDistance);
 
         // DRAWING CENTER ARC
         pen.beginPath();
         pen.strokeStyle = arc;
-        pen.arc(center.x, center.y, arcRadiuss, Math.PI, 2 * Math.PI);
+        pen.arc(center.x, center.y, arcRadius, Math.PI, 2 * Math.PI);
         pen.stroke();
 
         // DRAWING CENTRAL CIRCLE
-        pen.fillStyle = "white";
+        pen.fillStyle = "black";
         pen.beginPath();
         pen.arc(x, y, circleRadius, 0, 2 * Math.PI);
         pen.fill();
