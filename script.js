6feb8d3967459e58206a3ba2e36e389f56c33701
Gaspar Bonari <89667053+GasparBonari@users.noreply.github.com>
2023-07-29 13:01:17 +0200

    .

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 "use strict";
 
 // Array of colors for arcs
-const arcs = [
+const arcs = 
+[
   "#D0E7F5", "#D9E7F4", "#D6E3F4", "#BCDFF5", "#B7D9F4", "#C3D4F0",
   "#9DC1F3", "#9AA9F4", "#8D83EF", "#AE69F0", "#D46FF1", "#DB5AE7",
   "#D911Da", "#D601Cb", "#E713Bf", "#F24CAe", "#FB79AB", "#FFB6C1",
@@ -15,7 +16,8 @@ const pen = paper.getContext("2d");
 // Start time to calculate elapsed time
 let startTime = new Date().getTime();
 
-const draw = function() {
+const draw = function() 
+{
   // Set the canvas dimensions to match its container
   paper.width = paper.clientWidth;
   paper.height = paper.clientHeight;
@@ -40,7 +42,8 @@ const draw = function() {
   pen.lineTo(end.x, end.y);
   pen.stroke();
 
-  arcs.forEach((arc, index) => {
+  arcs.forEach((arc, index) => 
+  {
     const numberOfLoops = 50 - index;
     const oneFullLoop = 2 * Math.PI;
     const velocity = (oneFullLoop * numberOfLoops) / 900;
